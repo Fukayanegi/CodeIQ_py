edcfc5d4275b2f4fe2ffbe34d70fa78befd459e6
Fukayanegi <fukayanegi1981@gmail.com>
2016-10-15 22:52:56 +0900

    fix: 最小公倍数を因数の集合で求めるとある因数が2回登場すると成り立たない不具合を修正 #1

--- programing_club/reduction_of_fraction.py
+++ programing_club/reduction_of_fraction.py
@@ -21,29 +21,63 @@ def decomposite_prime(num):
         break
   return answer
 
+def diff_factor(factor1, factor2):
+  factor1.sort()
+  factor2.sort()
+  j = 0
+  answer = []
+  for i in range(0, len(factor1)):
+    if j < len(factor1) and factor2[i] > factor1[j]:
+      while factor2[i] > factor1[j]:
+        answer.append(factor1[j])
+        j += 1
+        if j >= len(factor1):
+          break
+    elif j < len(factor1) and factor2[i] == factor1[j]:
+      j += 1
+  if j < len(factor1):
+    answer += factor1[j:len(factor1)]
+  return answer
+
+def common_factor(factor1, factor2):
+  factor1.sort()
+  factor2.sort()
+  j = 0
+  answer = []
+  for i in range(0, len(factor1)):
+    if j < len(factor2) and factor1[i] == factor2[j]:
+      answer.append(factor1[i])
+      j += 1
+    elif j < len(factor2) and factor1[i] > factor2[j]:
+      while factor1[i] > factor2[j]:
+        j += 1
+        if j >= len(factor2):
+          break
+  return answer
+
 in1 = raw_input().split('/')
 in2 = raw_input().split('/')
 
 n1 = int(in1[0])
-d1 = set(decomposite_prime(int(in1[1])))
+d1 = decomposite_prime(int(in1[1]))
 n2 = int(in2[0])
-d2 = set(decomposite_prime(int(in2[1])))
+d2 = decomposite_prime(int(in2[1]))
 
-common_multiple_factor = iter(d1 | (d1 ^ d2))
+common_multiple_factor = iter(d1 + diff_factor(d2, d1))
 denominator = reduce(lambda x, y: x * y, common_multiple_factor)
+# print(denominator)
 
 if d1 != d2:
-  n1 = n1 * reduce(lambda x, y: x * y, (d2 - d1))
-  n2 = n2 * reduce(lambda x, y: x * y, (d1 - d2))
+  n1 = n1 * reduce(lambda x, y: x * y, diff_factor(d2, d1))
+  n2 = n2 * reduce(lambda x, y: x * y, diff_factor(d1, d2))
 numerator = n1 + n2
+# print(numerator)
 
-factor_n = set(decomposite_prime(numerator))
-factor_d = set(decomposite_prime(denominator))
-common_factor = reduce(lambda x, y: x * y, factor_n & factor_d)
+factor_n = decomposite_prime(numerator)
+factor_d = decomposite_prime(denominator)
+common_factor = reduce(lambda x, y: x * y, common_factor(factor_n, factor_d))
+# print(common_factor)
 if denominator / common_factor == 1:
   print(numerator / common_factor)
 else:
   print("%d/%d" % (numerator / common_factor, denominator / common_factor))
-
-
-
